f37151c20c546a475f45b45ef71c2725df9648bb
Suvojit Chanda <chanda.cdac@gmail.com>
2022-11-28 16:42:51 +0530

    adding the missing file

--- app/src/androidTest/java/com/org/skc/astronomypictureoftheday/MainActivityTest.java
+++ app/src/androidTest/java/com/org/skc/astronomypictureoftheday/MainActivityTest.java
@@ -2,6 +2,7 @@ package com.org.skc.astronomypictureoftheday;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.MediumTest;
@@ -25,10 +26,22 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void ensureButtonIsPresent() throws Exception {
+    public void ensureWidgetsArePresent() throws Exception {
         MainActivity mainActivity = rule.getActivity();
         View viewById = mainActivity.findViewById(R.id.darkMode);
         assertThat(viewById, notNullValue());
         assertThat(viewById, instanceOf(Button.class));
+
+        View tView = mainActivity.findViewById(R.id.textViewExplanation);
+        assertThat(tView, notNullValue());
+        assertThat(tView, instanceOf(TextView.class));
+
+        View tView2 = mainActivity.findViewById(R.id.textViewDatePicker);
+        assertThat(tView2, notNullValue());
+        assertThat(tView2, instanceOf(TextView.class));
+
+        View tView3 = mainActivity.findViewById(R.id.textViewSuggest);
+        assertThat(tView3, notNullValue());
+        assertThat(tView3, instanceOf(TextView.class));
     }
 }
